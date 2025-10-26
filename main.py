6c7382602757e6a02b64f21f144b264f13fd4841
Neil <evapatrick70@gmail.com>
2025-10-26 23:33:16 +0800

    "added capturing of text on specific part of page

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import pypdf
+import pymupdf  
 
 if __name__ == "__main__":
-    read_pdf = pypdf.PdfReader("D://automation//test data//ThirtyDaySummary_638966391093975385.pdf")
+    read_pdf = pymupdf.open("D://automation//test data//ThirtyDaySummary_638966391093975385.pdf")
     
-    page = read_pdf.pages[0]
+    page = read_pdf[0]
     
-    print(page.extract_text())
+    start = page.search_for("Compassionate Home Health LTD")
+    startTitle = page.search_for("30 DAY SUMMARY/CASE")
+    
+    end = page.search_for("Homebound Status")
+    
+    rx0 = start[0].x0
+    ry0 = start[0].y0
+    rx1 = end[0].x1
+    ry1 = end[0].y1
+    
+    cr = pymupdf.Rect(rx0,ry0,rx1,ry1)
+    
+    text = page.get_text(clip=cr, sort=True)
+    
+    print(text)
+    
+    #text to find : Patient Name, Patient DOB, Physician Name, Physician Phone, Physician Fax, Date on bottom, Date today, Number of pages + 2
